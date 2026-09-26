1d5cec0d37018f539d60b9fe9b35a672bb0f745f
Borinn <93236198+MauricioAriasVillanueva@users.noreply.github.com>
2026-09-26 03:14:04 -0500

    implementar hash SHA-256 y enmascaramiento de DNI

--- src/SeguridadUtil.java
+++ src/SeguridadUtil.java
@@ -6,7 +6,7 @@ import java.security.NoSuchAlgorithmException;
 
 public class SeguridadUtil {
 
-    public static String encriptarSHA256(String texto) {
+    public static String encriptarSHA256(String texto ) {
         try {
             MessageDigest digest = MessageDigest.getInstance("SHA-256");
             byte[] hash = digest.digest(texto.getBytes(StandardCharsets.UTF_8));
